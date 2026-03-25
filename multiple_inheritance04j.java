ddedb1f6880d1952e1f35857ef2d81ed0c5e4577
jishan <jk101793@gmail.com>
2026-03-25 07:45:37 +0530

    multipleinheritance in java

---
+++ multiple_inheritance04j.java
@@ -0,0 +1,56 @@
+// first problem
+// interface camera{
+//     public void clickphoto();
+// }
+
+// interface music{
+//     public void song();
+// }
+
+// interface gps{
+//     public void maps();
+// }
+
+//  class SmartDevice implements camera,music,gps{
+//     public void clickphoto(){
+//         System.out.println("photo clicked");
+//     }
+
+
+// public void song(){
+//     System.out.println("music are played");
+// }
+
+// public void maps(){
+//     System.out.println("maps are run ");
+// }
+
+//  }
+
+// 2nd problem 
+interface Googlelogin{
+    void gogle ();
+}
+interface Facebooklogin{
+    void facebook();
+}
+interface Instagram{
+    void insta();
+}
+
+public class multiple_inheritance04j {
+    public static void main(String[] args) {
+        
+//   SmartDevice device = new SmartDevice();
+  
+//   device.clickphoto();
+//   device.song();
+//   device.maps();
+
+
+
+
+
+
+    }
+}
