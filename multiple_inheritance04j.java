2a389dafb0b212ee59650a0e67929ce8c5ca998b
jishan <jk101793@gmail.com>
2026-04-12 08:59:30 +0530

    Added multiple inheritance using interfaces in Java

--- multiple_inheritance04j.java
+++ multiple_inheritance04j.java
@@ -1,56 +1,55 @@
-// first problem
-// interface camera{
-//     public void clickphoto();
-// }
-
-// interface music{
-//     public void song();
-// }
-
-// interface gps{
-//     public void maps();
-// }
-
-//  class SmartDevice implements camera,music,gps{
-//     public void clickphoto(){
-//         System.out.println("photo clicked");
-//     }
-
-
-// public void song(){
-//     System.out.println("music are played");
-// }
-
-// public void maps(){
-//     System.out.println("maps are run ");
-// }
-
-//  }
-
-// 2nd problem 
-interface Googlelogin{
-    void gogle ();
-}
-interface Facebooklogin{
-    void facebook();
-}
-interface Instagram{
-    void insta();
+//first problem
+interface camera{
+    public void clickphoto();
 }
 
-public class multiple_inheritance04j {
-    public static void main(String[] args) {
-        
-//   SmartDevice device = new SmartDevice();
-  
-//   device.clickphoto();
-//   device.song();
-//   device.maps();
+interface music{
+    public void song();
+}
 
+interface gps{
+    public void maps();
+}
 
-
-
-
-
+ class SmartDevice implements camera,music,gps{
+    public void clickphoto(){
+        System.out.println("photo clicked");
     }
+
+
+public void song(){
+    System.out.println("music are played");
 }
+
+public void maps(){
+    System.out.println("maps are run ");
+}
+ }
+
+ public class multiple_inheritance04j {
+ 
+    
+ 
+public static void main(String[] args) {
+    
+
+
+SmartDevice device = new SmartDevice(); 
+
+  device.clickphoto();
+        device.song();
+        device.maps();
+        
+}
+
+ }
+// 2nd problem 
+
+
+
+
+
+
+
+
+    
